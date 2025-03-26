14a710e331539af2c140bb1abd337ac32ba67e30
Striver20 <vermaashitiit@gmail.com>
2025-03-26 17:31:22 +0530

    made course backend

---
+++ backend/controllers/CourseController.js
@@ -0,0 +1,47 @@
+const courseModel = require("../models/CourseModel");
+
+const getCourseByCode = async (req, res) => {
+  try {
+    const { courseCode } = req.params;
+
+    const course = await courseModel.findOne({ courseCode });
+
+    if (!course) {
+      return res.staus(404).json({
+        success: false,
+        message: "Course not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Course fetched successfully",
+      course: course,
+    });
+  } catch (err) {
+    console.error("Error fetching course:", err.message);
+    return res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
+
+const getAllCourse = async (req, res) => {
+  try {
+    const courses = await courseModel.find({});
+    return res.status(200).json({
+      success: true,
+      message: "Courses fetched successfully",
+      courses: courses,
+    });
+  } catch (err) {
+    console.error("Error fetching courses:", err.message);
+    return res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
+
+module.exports = { getCourseByCode, getAllCourse };
---
+++ backend/routes/courseRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getCourseByCode,
+  getAllCourses,
+} = require("../controllers/StudentController");
+
+// Define the route to get a student's courses by their ID
+// GET /api/students/:id/courses
+router.get("/code/:courseCode", getCourseByCode);
+router.get("/allCourses", getAllCourses);
+
+module.exports = router;
